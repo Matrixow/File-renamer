c917cae85e40c5e80edf1dc0328c1d99195b3bea
Ernest <lollipop@MBP.local>
2024-08-17 22:15:59 +0000

    wrote folder selection code

--- ui.py
+++ ui.py
@@ -1,1 +1,27 @@
 import tkinter as tk
+from tkinter import filedialog
+
+class AppUI:
+    def __init__(self, windowTitle):
+        self.title = windowTitle
+
+    def select_folder():
+        folderPath = filedialog.askdirectory(title="Select a folder")
+        if folderPath:
+            print(f"Folder path is {folderPath}")
+        return
+
+    def runApp(self):
+        window = tk.Tk()
+        window.title(self.title)
+        window.geometry("500x300")
+        window.resizable(False, False)
+
+        button = tk.Button(text="Select a folder", command=self.select_folder())
+        label = tk.Label(text="Select folder directory")
+        label.pack()
+        window.mainloop() 
+
+
+app = AppUI("File renamer")
+app.runApp()
