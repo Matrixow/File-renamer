f9d8feb7a15505077b6c04ce1d16001ccb2348cb
Ernest <lollipop@MBP.local>
2024-08-18 01:52:13 +0000

    added the script for renaming the files

--- main.py
+++ main.py
@@ -7,10 +7,20 @@ class ItemRenamer:
         self.directory = directory
 
     def list_items(self):
-        items = []
+        items = ""
         for item in os.listdir(self.directory):
-            items.append(item)
-        return str(items)
+            items+=f"sep{item}"
+        return "\n".join(items.split("sep"))
+    
+    def rename_items(self, nametoremove):
+        self.nametoremove = nametoremove
+
+        os.chdir(self.directory)
+        for file in os.listdir(self.directory):
+            if self.nametoremove in file:
+                filename = file
+                newname = filename.replace(self.nametoremove, "")
+                os.rename(f"{self.directory}/{file}", newname)
 
 re = ItemRenamer("/Users/ernest/Documents/Movies/Anime/Dr Stone/S03/Cour1")
 re.list_items()
